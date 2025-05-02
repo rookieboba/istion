a2590ccba65c0bd173bf7aa32edb41810f5ffb5d
Sungbin Park <terranbin@gmail.com>
2025-05-02 11:08:08 +0000

    added public access

--- eks-practice/main.tf
+++ eks-practice/main.tf
@@ -26,6 +26,10 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
   enable_irsa = true
 
   eks_managed_node_groups = {
